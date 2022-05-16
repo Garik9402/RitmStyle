ac1d5f9fc843bfec40cfaf1d592112c0aec00c13
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-05-16 14:41:07 +0300

    add slider;)

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        speed: 1000,
+        autoplay: true,
+        centerMode: true,
+    });
+
+});
